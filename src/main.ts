dec85552276ad178a2eafe3ee2906b677fab82d2
p_yaossong <p_yaossong@tencent.com>
2023-07-11 16:40:06 +0800

    feat: nest init

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { urlencoded, json } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 // vercel
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
   app.setGlobalPrefix('api'); // 全局路由前缀
   app.use(cors()); // 允许跨域
   app.use(json({ limit: '10mb' })); // 允许上传大文件
   app.use(urlencoded({ extended: true, limit: '10mb' })); // 允许上传大文件
+  app.useStaticAssets(join(__dirname, '../static'), {
+    prefix: '/static',
+  }); // 静态资源
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
--- src/sharp/sharp.service.ts
+++ src/sharp/sharp.service.ts
@@ -4,11 +4,12 @@ import { UpdateSharpDto } from './dto/update-sharp.dto';
 import * as sharp from 'sharp'
 @Injectable()
 export class SharpService {
-  create(createSharpDto: CreateSharpDto) {
+  async create(createSharpDto: CreateSharpDto) {
     if(!createSharpDto.code) return {code: 400, msg: '参数错误'}
+
     const buffer = Buffer.from(createSharpDto.code)
-    const img = sharp(buffer).png().toFile('src/sharp/1.png')
-    console.log('img', img)
-    return {code: 200, img} || {code: 400, msg: '生成失败'};
+    const img = await sharp(buffer,{ limitInputPixels: 1000000000 }).png().toFile('./static/1.png')
+    return {code: 200, img, url: `http://localhost:3000/static/1.png`} || {code: 400, msg: '生成失败'};
+    
   }
 }
